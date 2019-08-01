28b4add0e550c9cadfc75a76ccfe3d75f02ee77a
Marcella <marcellasasso@gmail.com>
2019-08-01 11:58:28 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import InitAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import InitFuncionamento from './modules/funcionamento.js';
+import InitFetchAnimais from './modules/fetch-animais.js';
+import InitFetchBitcoin from './modules/fetch-bitcoin.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+InitAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+InitFuncionamento();
+InitFetchAnimais();
+InitFetchBitcoin();
